335e12f3b2100a656d2a932d6fdf895e80ac5ff4
SameepArya23 <sameeparya02@gmail.com>
2024-05-26 10:34:21 +0530

    change in functions

--- functions/api.js
+++ functions/api.js
@@ -2,16 +2,16 @@ const express = require("express");
 const serverless = require("serverless-http");
 const app = express();
 const router = express.Router();
-const { PeerServer } = require('peer');
-const port = process.env.PORT || 9000;
+// const { PeerServer } = require('peer');
+// const port = process.env.PORT || 9000;
 
 router.get("/", (req, res) => {
     res.send("App is running..");
 });
 
-const peerServer = PeerServer({ port: port, path: '/myapp' });
+// const peerServer = PeerServer({ port: port, path: '/myapp' });
 
-app.listen(port, () => console.log(`PeerJS server running on port ${port}`));
+// app.listen(port, () => console.log(`PeerJS server running on port ${port}`));
 
 app.use("/.netlify/functions/app", router);
 module.exports.handler = serverless(app);
